2d5ada67ab68d2702c0220be15c05254325a773d
Robert Fletcher <lobatifricha@gmail.com>
2025-10-15 21:14:11 -0700

    initial commit

---
+++ lib/baseliner.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "baseliner/version"
+
+module Baseliner
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/baseliner_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Baseliner do
+  it "has a version number" do
+    expect(Baseliner::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
