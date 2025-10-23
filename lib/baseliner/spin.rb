3f46fa2681b409c3645f0692d2e309cc4e82799e
Robert Fletcher <lobatifricha@gmail.com>
2025-10-23 15:29:03 -0700

    cycle spinners globally
    So the animation continues from where it left off with each new thread.

--- lib/baseliner/spin.rb
+++ lib/baseliner/spin.rb
@@ -3,15 +3,13 @@
 # module to display spinners in the terminal
 module Baseliner::Spin
   class << self
-    SPINNERS = YAML.load_file(File.join(__dir__, "spinners.yml"))
+    SPINNERS = YAML.load_file(File.join(__dir__, "spinners.yml")).map(&:cycle)
 
     # Displays a spinner in the terminal while the given thread is alive.
     def call(thread)
-      spinners = SPINNERS.map(&:cycle)
-
       while thread.alive?
         # hide cursor, clear line, and print spinner frame
-        print("\e[?25l\r#{spinners.map(&:next).join(" ")}")
+        print("\e[?25l\r#{SPINNERS.map(&:next).join(" ")}")
         sleep(0.1)
       end
 
